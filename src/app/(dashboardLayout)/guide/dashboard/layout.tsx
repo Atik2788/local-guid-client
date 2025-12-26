47628e484df88d0e31f897a727116a6e69161a27
atik2788 <ashanto653.2@gmail.com>
2025-12-26 14:42:25 +0600

    done dashboard Layout

---
+++ src/app/(dashboardLayout)/guide/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GuideDashboardLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <div>{children}</div>;
+};
+
+export default GuideDashboardLayout;
